37caac399d73721530cb9bc14d70a24f3f9cf193
ColtonLeese <coltleese@gmail.com>
2017-12-30 17:06:03 -0500

    adding init and gitignore, and starting hours first test

---
+++ baby_sitter_kata.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3 
+
+
+def getBabySitterStartHours(hours):
+	return null
+
+def main():
+	# getBabySitterStartHours(5):
+	return null
+
+
+if __name__  == '__main__':
+	main()
--- baby_sitter_kata_specs.py
+++ baby_sitter_kata_specs.py
@@ -1,10 +1,13 @@
-#import your_file_name
+#!/usr/bin/env python3 
+
+import baby_sitter_kata
 import unittest
 
-def yourFileTDD(unittest.TestCase):
+class babysitterKataTDD(unittest.TestCase):
 
-	def your_method_to_test(self):
-
+	def testBabySitterHoursGreaterThanOrEqualToSeventeenReturnsTrue(self):
+		self.assertEqual(True, baby_sitter_kata.getBabySitterStartHours(17))
+		self.assertEqual(True, baby_sitter_kata.getBabySitterStartHours(18))
 
 if __name__ == '__main__':
 	unittest.main()		
